b6fe7e44d38881edb97113104172b9c14d584183
@natalia.orlova <natalia.orlova@messaggio.com>
2022-03-31 23:20:24 +0300

    fix: changed finished page

--- src/layouts/ModalLayout.js
+++ src/layouts/ModalLayout.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/shared/Header'
 import Req from '../components/shared/Req';
 
 const ModalLayout = () => {
+    const location = useLocation();
     return (
         <div className='modal__open'>
             <Header />
             <div className='d-flex'>
                 <Outlet />
-                <Req />
-                </div>
+                {location.pathname.includes('/finished') ? null : <Req />}
+            </div>
         </div>
     )
 };
